9a0ec2c133e3d860cf78c1b9037d35a08134bfac
Matt Graham <mail@matg.dev>
2025-06-21 17:20:14 +1000

    Fix: Remove 'hid' property from meta tags for Nuxt 3 compatibility - Removed all 'hid' keys from meta tags in nuxt.config.ts to resolve type errors

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,58 +8,48 @@ export default defineNuxtConfig({
       meta: [
         { name: "viewport", content: "width=device-width, initial-scale=1" },
         {
-          hid: "description",
           name: "description",
           content:
             "Guess That Tune is an interactive music quiz web app. Test your knowledge of popular artists and songs with fun, dynamic quizzes!",
         },
         {
-          hid: "keywords",
           name: "keywords",
           content:
             "music quiz, guess the song, music game, artist quiz, song trivia, interactive quiz",
         },
-        { hid: "author", name: "author", content: "Guess That Tune" },
+        { name: "author", content: "Guess That Tune" },
         {
-          hid: "og:title",
           property: "og:title",
           content: "Guess That Tune - Music Quiz Game",
         },
         {
-          hid: "og:description",
           property: "og:description",
           content:
             "Challenge your music knowledge! Listen to song previews and guess the correct title in this interactive quiz.",
         },
-        { hid: "og:type", property: "og:type", content: "website" },
+        { property: "og:type", content: "website" },
         {
-          hid: "og:url",
           property: "og:url",
           content: "https://guessthattune.com",
         },
         {
-          hid: "og:image",
           property: "og:image",
           content: "https://guessthattune.com/og-image.png",
         },
         {
-          hid: "twitter:card",
           name: "twitter:card",
           content: "summary_large_image",
         },
         {
-          hid: "twitter:title",
           name: "twitter:title",
           content: "Guess That Tune - Music Quiz Game",
         },
         {
-          hid: "twitter:description",
           name: "twitter:description",
           content:
             "Test your knowledge of popular artists and songs with Guess That Tune!",
         },
         {
-          hid: "twitter:image",
           name: "twitter:image",
           content: "https://guessthattune.com/og-image.png",
         },
